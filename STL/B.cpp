9bf2bd695f5223029c62976d6c8a9d9bf081905a
Sudip Kumar <keya24gadu2020@gmail.com>
2024-06-10 00:15:10 -0800

    stl

--- STL/B.cpp
+++ STL/B.cpp
@@ -7,6 +7,40 @@ using namespace std;
 int main()
 {
     fio;
-   
+    list<ll> l;
+    auto cursor = l.end();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll c, d, x;
+        cin >> c;
+        if (c == 0)
+        {
+            cin >> x;
+            cursor = l.insert(cursor, x);
+        }
+        else if (c == 1)
+        {
+            cin >> d;
+            if (d > 0)
+            {
+                while (d-- && cursor != l.end())
+                    cursor++;
+            }
+            else if (d < 0)
+            {
+                while (d++ && cursor != l.begin())
+                    cursor--;
+            }
+        }
+        else if (c == 2)
+        {
+            cursor != l.end();
+            cursor = l.erase(cursor);
+        }
+    }
+    for (auto it = l.begin(); it != l.end(); ++it)
+        cout << *it << endl;
     return 0;
 }
--- STL/T.cpp
+++ STL/T.cpp
@@ -4,6 +4,29 @@
 //     ios_base::sync_with_stdio(false); \
 //     cin.tie(NULL)
 // #define ll long long
+// map<char, int> bracket = {{'(', -1}, {'[', -2}, {')', 1}, {']', 2}};
+// string balance(string s)
+// {
+//     stack<char> st;
+//     for (auto b : s)
+//     {
+//         if (bracket[b] < 0)
+//             st.push(b);
+//         else
+//         {
+//             if (st.empty())
+//                 return "No";
+//             char top = st.top();
+//             st.pop();
+//             if (bracket[top] + bracket[b] != 0)
+//                 return "No";
+//         }
+//     }
+//     if (st.empty())
+//         return "Yes";
+//     else
+//         return "No";
+// }
 // int main()
 // {
 //     fio;
@@ -13,18 +36,7 @@
 //     {
 //         string s;
 //         cin >> s;
-//         ll pr = 0, sq = 0;
-//         for (int i = 0; i < s.size(); ++i)
-//         {
-//             if (s[i] == '(' || s[i] == ')')
-//                 pr++;
-//             else if (s[i] == '[' || s[i] == ']')
-//                 sq++;
-//         }
-//         if ((pr % 2 == 0 && sq % 2 == 0) || s.size() == 0)
-//             cout << "Yes" << endl;
-//         else
-//             cout << "No" << endl;
+//         cout << balance(s) << endl;
 //     }
 
 //     return 0;
@@ -32,43 +44,39 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#define fio                           \
+#define fio                           
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL)
 #define ll long long
-map<char, int> bracket = {{'(', -1}, {'[', -2}, {')', 1}, {']', 2}};
-string balance(string s)
-{
-    stack<char> st;
-    for (auto b : s)
-    {
-        if (bracket[b] < 0)
-            st.push(b);
-        else
-        {
-            if (st.empty())
-                return "No";
-            char top = st.top();
-            st.pop();
-            if (bracket[top] + bracket[b] != 0)
-                return "No";
-        }
-    }
-    if (st.empty())
-        return "Yes";
-    else
-        return "No";
-}
+
 int main()
 {
     fio;
-    int t;
+    ll t;
     cin >> t;
+    stack<char> st;
     while (t--)
     {
         string s;
         cin >> s;
-        cout << balance(s) << endl;
+        if (s.size() == 0)
+            cout << "Yes" << endl;
+        int i;
+        for (i = 0; i < s.size(); ++i)
+        {
+            if (s[i] == '(' || s[i] == '[')
+                st.push(s[i]);
+            else if (!st.empty() && ((st.top() == '(') && (s[i] == ')') || (st.top() == '[') && s[i] == ']'))
+                st.pop();
+            else if (s[i] == ' ')
+                continue;
+            else
+                break;
+        }
+        if (st.empty() && s.size() == i)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
     }
 
     return 0;
