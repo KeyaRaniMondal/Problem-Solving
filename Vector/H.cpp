d88b92a84c2d8e1b50c0020812d2106fd1347544
Sudip Kumar <keya24gadu2020@gmail.com>
2025-05-02 23:28:28 +0600

    vector

---
+++ Vector/H.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n <= 1)
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v;
+
+    for (int i = 0; i < n; ++i)
+    {
+        v.push_back(fibonacci(i));
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
